4c1642bc0f9f6822c6af77603e320ab2fbcfe679
lownotelee <lownotelee@gmail.com>
2021-01-11 21:55:33 +1100

    Initial Commit

---
+++ SwiftUI-Appetizers/SwiftUI_AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppetizersApp.swift
+//  SwiftUI-Appetizers
+//
+//  Created by Lee on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
